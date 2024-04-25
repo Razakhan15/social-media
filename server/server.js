edddfd97bb65a1b8676c55e990f5644cc2317610
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-04-25 22:40:49 +0530

    finished

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 connectDb();
+const corsOptions = {
+    origin: "https://social-media-client-nsfh.onrender.com"
+}
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
